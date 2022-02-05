dcd795e29d5a76b570a245b78d217b2b20b1f1c8
Yogesh  Sambare <bloggingscan@gmail.com>
2022-02-05 21:13:51 +0530

    update

--- array/commonelement.js
+++ array/commonelement.js
@@ -1,3 +1,11 @@
 // duplicatearray
 
-// find common eleme
+// find common elememet in three sorte array
+
+var arr1 = [1,2,3,4,5,6,7,8,60,];
+var arr2 = [2,8,20,32,60,63,65,80,85];
+var arr3 = [3,20,33,44,60,68,69,75,100]
+
+out = [];
+
+for (var i =0; i <= arr1.len)
