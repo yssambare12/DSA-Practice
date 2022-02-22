ec2792f1dd85e40ac2f1c3e90495fcf3a14b562c
Yogesh  Sambare <bloggingscan@gmail.com>
2022-02-22 22:49:09 +0530

    update

---
+++ array/reversearybym.js
@@ -0,0 +1,13 @@
+// You have given an array reverse the array after given index number
+// k = 2
+
+var arr = [1,2,3,4,5,6,7];
+var k = 3;
+var end = arr.length;
+
+console.log(arr.length)
+
+for(var i = end; i >= k; i--){
+  console.log(arr[i])
+ }
+
