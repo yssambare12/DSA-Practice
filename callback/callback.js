7fe12b2b54d6711b432ac68044990da6dc91a419
Yogesh  Sambare <bloggingscan@gmail.com>
2022-02-27 23:45:19 +0530

    Update callback.js

--- callback/callback.js
+++ callback/callback.js
@@ -1,13 +1,13 @@
-setTimeout (function () {
-    console.log("Timerr")
-    }, 3000)
+// setTimeout (function () {
+//     console.log("Timerr")
+//     }, 3000)
     
-    function x(y){
-        console.log("x");
-        y();
-    }
+//     function x(y){
+//         console.log("x");
+//         y();
+//     }
     
-    x(function y(){
-        console.log("y")
-    });
+//     x(function y(){
+//         console.log("y")
+//     });
     
