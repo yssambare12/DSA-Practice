882f42ba2dccad4775e399b564e1b377e5e00073
Yogesh  Sambare <bloggingscan@gmail.com>
2022-02-27 23:47:52 +0530

    Update promises.js

--- promises/promises.js
+++ promises/promises.js
@@ -1,18 +1,18 @@
-function prom(complete) {
-    return new Promise(function(resolve,reject){
-        console.log("Fetching data. Please wait")
-        setTimeout(() => {
-            if(complete){
-                resolve("i am sucessfull");
-            }else{
-                reject("i am failed")
-            }
-        }, 3000);
-    });
-}
+// function prom(complete) {
+//     return new Promise(function(resolve,reject){
+//         console.log("Fetching data. Please wait")
+//         setTimeout(() => {
+//             if(complete){
+//                 resolve("i am sucessfull");
+//             }else{
+//                 reject("i am failed")
+//             }
+//         }, 3000);
+//     });
+// }
 
-prom(false).then((result) => {
-    console.log(result);
-}).catch((error) => {
-    console.log(error)
-});
+// prom(false).then((result) => {
+//     console.log(result);
+// }).catch((error) => {
+//     console.log(error)
+// });
