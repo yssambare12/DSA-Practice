a45d07756094ce347019d568331c470d526a8691
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-08 23:19:12 +0530

    chore:resolved max consecutive problem

---
+++ Max-Consecutive-Ones-484.js
@@ -0,0 +1,20 @@
+// 485. Max Consecutive Ones
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let currentcount = 0;
+    let maxcount = 0;
+  
+    for (let i = 0; i < nums.length; i++){
+      if(nums[i] == 1){
+          currentcount++;
+      } else {
+          maxcount = Math.max(currentcount, maxcount);
+          currentcount = 0;
+      }
+    }  
+    return Math.max(currentcount, maxcount);
+  };
