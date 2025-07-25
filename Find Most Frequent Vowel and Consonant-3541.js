c123428cfc5c2b0eed3970e159da51f7f6c038b5
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-25 23:50:25 +0530

    chore:resolved most frequency vovel and consonents problem

---
+++ Find Most Frequent Vowel and Consonant-3541.js
@@ -0,0 +1,59 @@
+// 3541. Find Most Frequent Vowel and Consonant
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given a string s consisting of lowercase English letters ('a' to 'z').
+
+// Your task is to:
+
+// Find the vowel (one of 'a', 'e', 'i', 'o', or 'u') with the maximum frequency.
+// Find the consonant (all other letters excluding vowels) with the maximum frequency.
+// Return the sum of the two frequencies.
+
+// Note: If multiple vowels or consonants have the same maximum frequency, you may choose any one of them. If there are no vowels or no consonants in the string, consider their frequency as 0.
+
+// The frequency of a letter x is the number of times it occurs in the string.
+
+// Example 1:
+
+// Input: s = "successes"
+
+// Output: 6
+
+// Explanation:
+
+// The vowels are: 'u' (frequency 1), 'e' (frequency 2). The maximum frequency is 2.
+// The consonants are: 's' (frequency 4), 'c' (frequency 2). The maximum frequency is 4.
+// The output is 2 + 4 = 6.
+
+function findMaxFrequency(s) {
+  let hash = {};
+  for (let i = 0; i < s.length; i++) {
+    if (!hash[s[i]]) {
+      hash[s[i]] = 1;
+    } else {
+      hash[s[i]]++;
+    }
+  }
+
+  let maxvo = 0;
+  let maxco = 0;
+  let vovels = ["a", "e", "i", "o", "u"];
+
+  for (let i = 0; i < s.length; i++) {
+    if (vovels.includes([s[i]])) {
+      if (hash[s[i]] > maxvo) {
+        maxvo = hash[s[i]];
+      }
+    } else {
+      if (hash[s[i]] > maxco) {
+        maxco = hash[s[i]];
+      }
+    }
+  }
+
+  return maxco + maxvo;
+}
