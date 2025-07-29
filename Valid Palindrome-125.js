071418a491555b1e057cbea5f5e818afaaefd1ab
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-29 07:40:01 +0530

    chore:resolved valid palidrome question

--- Valid Palindrome-125.js
+++ Valid Palindrome-125.js
@@ -18,3 +18,24 @@
 // Input: s = "race a car"
 // Output: false
 // Explanation: "raceacar" is not a palindrome.
+
+
+function validPalidrome(s){
+    s = s.toLowerCase();
+    let start = 0;
+    let end = s.length-1;
+
+    while(start < end){
+        if(!s[start].match(/[a-z0-9]/)){
+            ++start;
+        } else if(!s[end].match(/[a-z0-9]/)){
+            --end;
+        } else if(s[start] === s[end]){
+            start++;
+            end--;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
