9e0b5a22ae096462a1121f77ee9f29c81a00f688
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-29 23:59:48 +0530

    chore:formatted

--- array-sum-recurssion.js
+++ array-sum-recurssion.js
@@ -1,15 +1,13 @@
-let arr = [5,5,4,5,5,4,5,5];
+let arr = [5, 5, 4, 5, 5, 4, 5, 5];
 
 function sum(n) {
+  let isodd = arr[n] % 2 == 0;
 
-    let isodd = arr[n] % 2 == 0;
+  if (n == 0) {
+    return isodd ? arr[n] : 0;
+  }
 
-    if (n == 0) {
-        return isodd ? arr[n] : 0;
-    }
-
-    return isodd ? arr[n] + sum(n - 1) : sum(n - 1);
-
+  return isodd ? arr[n] + sum(n - 1) : sum(n - 1);
 }
 
-console.log(sum(arr.length-1));
+console.log(sum(arr.length - 1));
