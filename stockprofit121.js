824ce6a7129881cb52c01a8d873984606a919b57
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-29 23:58:46 +0530

    chore:formatted

--- stockprofit121.js
+++ stockprofit121.js
@@ -4,20 +4,18 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    let x = 0;
-    for (let i = 0; i < prices.length; i++){
-        for (let j = i+1; j< prices.length; j++){
-            let profit = prices[j] - prices[i]
-            if (profit > x){
-                x = profit;
-            }
-        }
+var maxProfit = function (prices) {
+  let x = 0;
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      let profit = prices[j] - prices[i];
+      if (profit > x) {
+        x = profit;
+      }
     }
-    return x;
+  }
+  return x;
 };
-
-
 
 // optimized approch
 
@@ -25,20 +23,18 @@ var maxProfit = function(prices) {
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-
-    let min = prices[0];
-    let maxProfit = 0;
-    for (let i = 1; i < prices.length; i++){
-        if(prices[i] - min > maxProfit){
-            maxProfit = prices[i]-min;
-        }
-    
-        if (prices[i]< min){
-            min = prices[i];
-        }
+var maxProfit = function (prices) {
+  let min = prices[0];
+  let maxProfit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] - min > maxProfit) {
+      maxProfit = prices[i] - min;
     }
-    
-    return maxProfit;
-    
-    };
+
+    if (prices[i] < min) {
+      min = prices[i];
+    }
+  }
+
+  return maxProfit;
+};
