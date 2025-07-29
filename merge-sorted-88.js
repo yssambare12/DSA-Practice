b87840f85f62bb7089d41033b2479a532b9419f2
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-29 23:58:29 +0530

    chore:formatted

--- merge-sorted-88.js
+++ merge-sorted-88.js
@@ -11,8 +11,6 @@
 
 // The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
- 
-
 // Example 1:
 
 // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
@@ -33,8 +31,6 @@
 // The result of the merge is [1].
 // Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
 
-
-
 /**
  * @param {number[]} nums1
  * @param {number} m
@@ -42,18 +38,18 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-    let p1 = m-1;
-    let p2 = n-1;
+var merge = function (nums1, m, nums2, n) {
+  let p1 = m - 1;
+  let p2 = n - 1;
 
-    for (let i = m+n-1; i>= 0; i--){
-        if (p2 < 0) break;
-        if(p1 >= 0 && (nums1[p1] > nums2[p2])){
-         nums1[i] = nums1[p1];
-         p1--;
-        } else {
-           nums1[i] = nums2[p2];
-           p2--;
-        }
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (p2 < 0) break;
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[i] = nums1[p1];
+      p1--;
+    } else {
+      nums1[i] = nums2[p2];
+      p2--;
     }
+  }
 };
