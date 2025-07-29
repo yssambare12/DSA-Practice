bba8379d24f8ed060bee252118cf0781016e8a36
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-07-29 23:59:20 +0530

    chore: formatted

--- Valid Palindrome-125.js
+++ Valid Palindrome-125.js
@@ -19,23 +19,22 @@
 // Output: false
 // Explanation: "raceacar" is not a palindrome.
 
+function validPalidrome(s) {
+  s = s.toLowerCase();
+  let start = 0;
+  let end = s.length - 1;
 
-function validPalidrome(s){
-    s = s.toLowerCase();
-    let start = 0;
-    let end = s.length-1;
-
-    while(start < end){
-        if(!s[start].match(/[a-z0-9]/)){
-            ++start;
-        } else if(!s[end].match(/[a-z0-9]/)){
-            --end;
-        } else if(s[start] === s[end]){
-            start++;
-            end--;
-        } else {
-            return false;
-        }
+  while (start < end) {
+    if (!s[start].match(/[a-z0-9]/)) {
+      ++start;
+    } else if (!s[end].match(/[a-z0-9]/)) {
+      --end;
+    } else if (s[start] === s[end]) {
+      start++;
+      end--;
+    } else {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
