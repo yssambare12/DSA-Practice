6eb4ca58842d947f0e8c12a1a944ef382e6a716c
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-02 17:23:48 +0530

    chore: updated file and formatting changes

--- Max-Consecutive-Ones-484.js
+++ Max-Consecutive-Ones-484.js
@@ -4,17 +4,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxConsecutiveOnes = function(nums) {
-    let currentcount = 0;
-    let maxcount = 0;
-  
-    for (let i = 0; i < nums.length; i++){
-      if(nums[i] == 1){
-          currentcount++;
-      } else {
-          maxcount = Math.max(currentcount, maxcount);
-          currentcount = 0;
-      }
-    }  
-    return Math.max(currentcount, maxcount);
-  };
+var findMaxConsecutiveOnes = function (nums) {
+  let currentcount = 0;
+  let maxcount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      currentcount++;
+    } else {
+      maxcount = Math.max(currentcount, maxcount);
+      currentcount = 0;
+    }
+  }
+  return Math.max(currentcount, maxcount);
+};
