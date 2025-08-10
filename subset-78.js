939ea4dfbc04f3b638befe1db1d39c5f9602c0d1
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-10 22:04:19 +0530

    CHORE:resolved subset problem

---
+++ subset-78.js
@@ -0,0 +1,41 @@
+// 78. Subsets
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [[],[0]]
+
+//NOT SOLVED NEED TO UNDERSTAND THE LOGIC
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+  let result = [];
+
+  function backtrack(start, current) {
+    result.push([...current]);
+
+    for (let i = start; i < nums.length; i++) {
+      current.push(nums[i]);
+      backtrack(i + 1, current);
+      current.pop();
+    }
+  }
+
+  backtrack(0, []);
+  return result;
+};
