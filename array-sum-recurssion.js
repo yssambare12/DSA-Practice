b62c3497dbffcf6fbb262cd93d78127b8bcd34c2
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-11 22:21:56 +0530

    chore:resolved recurssion array sum problem

--- array-sum-recurssion.js
+++ array-sum-recurssion.js
@@ -1,13 +1,27 @@
-let arr = [5, 5, 4, 5, 5, 4, 5, 5];
+// Sum of all numbers in an array without using for loop = recurssion should be preferable
 
 function sum(n) {
-  let isodd = arr[n] % 2 == 0;
+  let isodd = n % 2 == 1;
+  if (n == 0)
+    return {
+      if(isodd) {
+        return arr[n];
+      },
+    };
 
-  if (n == 0) {
-    return isodd ? arr[n] : 0;
-  }
-
-  return isodd ? arr[n] + sum(n - 1) : sum(n - 1);
+  return isodd ? arr[n] + sum(n - 1) : 0 + sum(n - 1);
 }
 
-console.log(sum(arr.length - 1));
+// Other similer approch
+
+function sum(n) {
+  if (n == 0) {
+    return arr[n];
+  }
+
+  if (n % 2 == 1) {
+    return arr[n] + sum(n - 1);
+  } else {
+    return 0 + sum(n - 1);
+  }
+}
