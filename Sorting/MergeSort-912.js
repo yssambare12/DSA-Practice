dea39a413f33b7c55fc67f77c37fcc97878cf730
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-18 10:51:43 +0530

    chore:resolved merge sort problem

---
+++ Sorting/MergeSort-912.js
@@ -0,0 +1,52 @@
+// 912. Sort an Array
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given an array of integers nums, sort the array in ascending order and return it.
+
+// You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+// Example 1:
+
+// Input: nums = [5,2,3,1]
+// Output: [1,2,3,5]
+// Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+// Example 2:
+
+// Input: nums = [5,1,1,2,0,0]
+// Output: [0,0,1,1,2,5]
+// Explanation: Note that the values of nums are not necessarily unique.
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return sort(left, right);
+}
+
+// Left = [1,3,5,6,8,9]
+// Right = [2,4,6,7]
+
+// res = [1,2,3,4,5,6,6,7 ]
+function sort(left, right) {
+  let res = [];
+  let i = 0;
+  let j = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      res.push(left[i]);
+      i++;
+    } else {
+      res.push(right[j]);
+      j++;
+    }
+  }
+
+  return [...res, ...left.slice(i), ...right.slice(j)];
+}
