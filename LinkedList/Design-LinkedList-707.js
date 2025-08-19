28eff1322c19edeafee8a3c2367334eb7a4c48af
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-19 22:13:55 +0530

    chore: design the linked list

---
+++ LinkedList/Design-LinkedList-707.js
@@ -0,0 +1,158 @@
+// 707. Design Linked List
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+// A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+// If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+// Implement the MyLinkedList class:
+
+// MyLinkedList() Initializes the MyLinkedList object.
+// int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+// void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+// void addAtTail(int val) Append a node of value val as the last element of the linked list.
+// void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
+// void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
+
+// Example 1:
+
+// Input
+// ["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+// [[], [1], [3], [1, 2], [1], [1], [1]]
+// Output
+// [null, null, null, null, 2, null, 3]
+
+// Explanation
+// MyLinkedList myLinkedList = new MyLinkedList();
+// myLinkedList.addAtHead(1);
+// myLinkedList.addAtTail(3);
+// myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+// myLinkedList.get(1);              // return 2
+// myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+// myLinkedList.get(1);              // return 3
+
+// Constraints:
+
+// 0 <= index, val <= 1000
+// Please do not use the built-in LinkedList library.
+// At most 2000 calls will be made to get, addAtHead, addAtTail, addAtIndex and deleteAtIndex.
+
+// Create a Node
+
+function Node(val) {
+  this.val = val;
+  this.next = null;
+}
+
+var MyLinkedList = function () {
+  this.head = null;
+  this.size = 0;
+};
+
+/**
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function (index) {
+  if (index < 0 || index >= this.size) {
+    return -1;
+  }
+  let curr = this.head;
+  for (let i = 0; i < index; i++) {
+    curr = curr.next;
+  }
+  return curr.val;
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function (val) {
+  let newnode = new Node(val);
+  newnode.next = this.head;
+  this.head = newnode;
+  this.size++;
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function (val) {
+  let newnode = new Node(val);
+
+  if (this.head === null) {
+    this.head = newnode;
+    this.size++;
+  } else {
+    let curr = this.head;
+    while (curr.next != null) {
+      curr = curr.next;
+    }
+    curr.next = newnode;
+    this.size++;
+  }
+};
+
+/**
+ * @param {number} index
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function (index, val) {
+  if (index < 0 || index > this.size) return;
+  let newnode = new Node(val);
+
+  if (index === 0) {
+    newnode.next = this.head;
+    this.head = newnode;
+    this.size++;
+    return;
+  } else if (index === this.size) {
+    this.addAtTail(val);
+    return;
+  } else {
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+    newnode.next = curr.next;
+    curr.next = newnode;
+    this.size++;
+  }
+};
+
+/**
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function (index) {
+  if (index < 0 || index >= this.size) return;
+
+  if (index === 0) {
+    this.head = this.head.next;
+    this.size--;
+    return;
+  }
+
+  let curr = this.head;
+  for (let i = 0; i < index - 1; i++) {
+    curr = curr.next;
+  }
+  curr.next = curr.next.next;
+  this.size--;
+};
+
+/**
+ * Example usage:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
