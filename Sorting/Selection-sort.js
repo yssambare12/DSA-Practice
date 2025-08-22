2d848a7f64bdf8efd287097f6c2f75194c05b900
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-22 23:56:46 +0530

    chore:files added to folder

--- Sorting/Selection-sort.js
+++ Sorting/Selection-sort.js
@@ -1,23 +1,20 @@
-// Sort this array using selection sort technique.
-
-let arr = [34, 54, 65, 34, 23, 4, 65, 32, 3154, 67, 784, 3, 2345, 67];
-
-function selectionSort(arr) {
-  let n = arr.legth;
-  for (let i = 0; i < n; i++) {
+function selectionsort(a) {
+  for (let i = 0; i < a.length - 1; i++) {
     let min = i;
-    for (let j = i + 1; j < n; j++) {
+    for (let j = i + 1; j < a.length; j++) {
       if (arr[j] < arr[min]) {
         min = j;
       }
     }
-    let temp = arr[i];
-    arr[i] = arr[min];
-    arr[min] = temp;
+    if (min != i) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
   }
-  return arr;
+  return a;
 }
 
-let result = selectionSort(arr);
+let arr = [7, 1, 2, 5, 7, 4, -8, 9, 0];
 
-console.log(selectionSort);
+let result = selectionsort(arr);
