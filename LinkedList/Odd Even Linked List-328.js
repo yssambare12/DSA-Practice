95d6e7591f7845465573c1acb0c5ed3edbd50f44
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-08-24 22:31:38 +0530

    chore:resolved odd even node problem again

--- LinkedList/Odd Even Linked List-328.js
+++ LinkedList/Odd Even Linked List-328.js
@@ -17,18 +17,21 @@
 // Input: head = [1,2,3,4,5]
 // Output: [1,3,5,2,4]
 
-function oddEvenLinedList(head) {
+function oddEvenLinkList(head) {
+  //corner case
   if (!head || !head.next) return head;
-  let odd = head;
-  even = head.next;
-  evenStart = even;
-  while (odd.next && even.next) {
-    odd.next = odd.next.next;
-    even.next = even.next.next;
-    odd = odd.next;
-    even = even.next;
+
+  let oddNode = head;
+  let evenNode = head.next;
+  let evenStart = evenNode;
+
+  while (oddNode.next && evenNode.next) {
+    oddNode.next = oddNode.next.next;
+    evenNode.next = evenNode.next.next;
+    oddNode = oddNode.next;
+    evenNode = evenNode.next;
   }
 
-  odd.next = evenStart;
+  oddNode.next = evenNode;
   return head;
 }
