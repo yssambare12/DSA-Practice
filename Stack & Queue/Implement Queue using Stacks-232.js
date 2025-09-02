c261b9db8b651be04a0819bfe4f12c6bf2be1d3d
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-02 08:39:10 +0530

    chore: resolved stack problem

---
+++ Stack & Queue/Implement Queue using Stacks-232.js
@@ -0,0 +1,57 @@
+// 232. Implement Queue using Stacks
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Implement a first in first out [FIFO) queue using o]nly two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+// Implement the MyQueue class:
+
+// void push(int x) Pushes element x to the back of the queue.
+// int pop() Removes the element from the front of the queue and returns it.
+// int peek() Returns the element at the front of the queue.
+// boolean empty() Returns true if the queue is empty, false otherwise.
+// Notes:
+
+// You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+// Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+// Example 1:
+
+// Input
+// ["MyQueue", "push", "push", "peek", "pop", "empty"]
+// [[], [1], [2], [], [], []]
+// Output
+// [null, null, null, 1, 1, false]
+
+function MyQueue() {
+  this.s1 = [];
+  this.s2 = [];
+}
+
+function QueuePush(x) {
+  this.s1.push(x);
+}
+
+function QueuePop() {
+  if (this.s2.length === 0) {
+    while (this.s1.length) {
+      this.s2.push(this.s1.pop());
+    }
+  }
+  return this.s2.pop();
+}
+
+function QueuePeak() {
+  if (this.s2.length === 0) {
+    while (this.s1.length) {
+      this.s2.push(this.s1.pop());
+    }
+  }
+  return this.s2[this.s2.length - 1];
+}
+
+function isEmptyCheck() {
+  return this.s1.length === 0 && this.s2.length === 0;
+}
