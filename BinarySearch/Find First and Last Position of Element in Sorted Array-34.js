d5ded8b69b1c6c161f5dc473feb1f11613420452
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-18 08:31:41 +0530

    resolved: first and last position with another approch

--- BinarySearch/Find First and Last Position of Element in Sorted Array-34.js
+++ BinarySearch/Find First and Last Position of Element in Sorted Array-34.js
@@ -57,3 +57,44 @@ var searchRange = function (nums, target) {
 
   return res;
 };
+
+// Another approch
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let ans = [-1, -1];
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) {
+      ans[0] = mid;
+      right = mid - 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  left = 0;
+  right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) {
+      ans[1] = mid;
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return ans;
+};
