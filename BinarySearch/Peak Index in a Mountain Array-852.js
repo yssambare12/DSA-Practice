70d49c4afe1611e6af50b5927718ae77d4b8eecd
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-18 09:01:54 +0530

    resolved: mountain array problem

---
+++ BinarySearch/Peak Index in a Mountain Array-852.js
@@ -0,0 +1,43 @@
+// 852. Peak Index in a Mountain Array
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+
+// Return the index of the peak element.
+
+// Your task is to solve it in O(log(n)) time complexity.
+
+// Example 1:
+
+// Input: arr = [0,1,0]
+
+// Output: 1
+
+// Example 2:
+
+// Input: arr = [0,2,1,0]
+
+// Output: 1
+
+// Example 3:
+
+// Input: arr = [0,10,5,2]
+
+// Output: 1
+
+function mountainArray(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (arr[mid + 1] > arr[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+}
