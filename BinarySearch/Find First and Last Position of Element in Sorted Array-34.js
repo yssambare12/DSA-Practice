02ba028d088726b1a724b12ce86752b5b732c549
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-18 08:01:18 +0530

    Create Find First and Last Position of Element in Sorted Array-34.js

---
+++ BinarySearch/Find First and Last Position of Element in Sorted Array-34.js
@@ -0,0 +1,59 @@
+// 34. Find First and Last Position of Element in Sorted Array
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+// If target is not found in the array, return [-1, -1].
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+// Example 2:
+
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+// Example 3:
+
+// Input: nums = [], target = 0
+// Output: [-1,-1]
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let res = [-1, -1];
+
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  if (nums[left] !== target) return res;
+  res[0] = left;
+
+  right = nums.length - 1;
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] <= target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  res[1] = right;
+
+  return res;
+};
