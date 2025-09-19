9b40dac1fe38de39290d636ce5575a5343c9130d
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-19 23:33:46 +0530

    chore: easy solution

--- BinarySearch/Single Element in a Sorted Array-540.js
+++ BinarySearch/Single Element in a Sorted Array-540.js
@@ -18,3 +18,57 @@
 
 // Input: nums = [3,3,7,7,10,11,11]
 // Output: 10
+
+// bit hard to implement solution
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] === nums[mid - 1]) {
+      let leftcount = mid - left + 1;
+      if (leftcount % 2 === 0) {
+        left = mid + 1;
+      } else {
+        right = mid - 2;
+      }
+    } else if (nums[mid] === nums[mid + 1]) {
+      let rightcount = right - mid + 1;
+      if (rightcount % 2 === 0) {
+        right = mid - 1;
+      } else {
+        left = mid + 2;
+      }
+    } else {
+      return nums[mid];
+    }
+  }
+};
+
+// bit easy to implement solution
+var singleNonDuplicate = function (nums) {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    // ensure mid is even
+    if (mid % 2 === 1) mid--;
+
+    if (nums[mid] === nums[mid + 1]) {
+      // unique is on the right
+      left = mid + 2;
+    } else {
+      // unique is on the left (including mid)
+      right = mid;
+    }
+  }
+  return nums[left];
+};
