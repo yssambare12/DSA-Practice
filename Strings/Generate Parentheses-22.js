9be90fa0019e956dd588825ba3b4409ba59432a1
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-21 23:11:24 +0530

    optimized approch

--- Strings/Generate Parentheses-22.js
+++ Strings/Generate Parentheses-22.js
@@ -30,3 +30,24 @@ function generateParenthesisBruteForce(n) {
 
 // Example
 console.log(generateParenthesisBruteForce(3));
+
+// oPTIMISED approcgh
+
+function generateParenthesis(n) {
+  let result = [];
+
+  function backtrack(current, open, close) {
+    if (current.length === 2 * n) {
+      result.push(current);
+      return;
+    }
+    if (open < n) backtrack(current + "(", open + 1, close);
+    if (close < open) backtrack(current + ")", open, close + 1);
+  }
+
+  backtrack("", 0, 0);
+  return result;
+}
+
+// Example
+console.log(generateParenthesis(3));
