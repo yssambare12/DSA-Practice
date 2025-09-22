6870086f1550ce146beaec07e71ebfb6673d3b65
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-09-22 08:25:32 +0530

    chore: resolved 2 sum problem with map approch

---
+++ Maps/two-sum-2.js
@@ -0,0 +1,36 @@
+// 1. Two Sum
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+function twoSum(nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    map[nums[i]] = i;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    let pair = target - nums[i];
+    if (map[pair] && map[pair] != i) {
+      return [i, map[pair]];
+    }
+  }
+}
